1611e65c04bddbb692746f11245b967245aa9c23
Revan <revan@ualberta.ca>
2020-11-12 10:56:24 -0700

    rm windows install

--- windows_install.cmd
+++
@@ -1,7 +0,0 @@
-py -m pip install --upgrade pip &
-py -m pip install --user virtualenv &
-py -m venv venv &
-venv\Scripts\Activate.ps1 &
-pip install -r requirements_windows.txt & 
-pip install -f https://github.com/Kojoley/atari-py/releases atari_py &
-pip install -e .
