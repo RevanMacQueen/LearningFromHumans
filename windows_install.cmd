a54a0bad15387eaee30382eb3294d0302b223f55
Revan <revan@ualberta.ca>
2020-11-12 10:35:35 -0700

    fixed windows install commmand

--- windows_install.cmd
+++ windows_install.cmd
@@ -1,8 +1,7 @@
 py -m pip install --upgrade pip &
 py -m pip install --user virtualenv &
 py -m venv venv &
-Set-ExecutionPolicy Unrestricted -Scope Process &
-.\venv\Scripts\activate &
+venv\Scripts\Activate.bat &
 pip install -r requirements_windows.txt & 
 pip install -f https://github.com/Kojoley/atari-py/releases atari_py &
 pip install -e .
