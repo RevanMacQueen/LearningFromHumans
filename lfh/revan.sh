fd0474be432aade685f278ba9a24ac8f371100df
Revan <revan@ualberta.ca>
2020-12-02 10:44:57 -0700

    fixed scripts

--- lfh/revan.sh
+++ lfh/revan.sh
@@ -4,6 +4,8 @@
 #SBATCH --time=14:00:00
 #SBATCH --mem=12G 
 #SBATCH --gres=gpu:1 
+#SBATCH --mail-user=revan@ualberta.ca
+#SBATCH --mail-type=ALL
 #SBATCH --output=slurm/%x-%j.out
 
 eval $(head -n $SLURM_ARRAY_TASK_ID experiments_revan.txt | tail -n 1)
