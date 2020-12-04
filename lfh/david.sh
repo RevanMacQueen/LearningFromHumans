551e0dad948e940f36b4f629e351bbcfde355a01
Revan <revan@ualberta.ca>
2020-12-04 14:51:26 -0700

    changed david.sh

--- lfh/david.sh
+++ lfh/david.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=def-jpineau
+#SBATCH --account=def-jrwright
 #SBATCH --array=1-54
 #SBATCH --time=02-12:00
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1 
 #SBATCH --output=slurm/%x-%j.out
-#SBATCH --mail-user=ruo.tao@mail.mcgill.ca
+#SBATCH --mail-user=revan@ualberta.ca
 #SBATCH --mail-type=ALL
 
 eval $(head -n $SLURM_ARRAY_TASK_ID experiments_david.txt | tail -n 1)
